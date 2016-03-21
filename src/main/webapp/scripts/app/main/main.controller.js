02b2b173de876ed7cd50848cb0334f3819d6b684
Caroliina Laantee <claantee@gmail.com>
2016-03-21 23:08:12 +0200

    feature/CV-7: Show 3 first and 3 last updated Curriculum Vitaes on front page

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -1,9 +1,42 @@
 'use strict';
 
 angular.module('resourcyApp')
-    .controller('MainController', function ($scope, Principal) {
+    .controller('MainController', function ($scope, Principal, $http) {
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+
+        $http({
+            method: 'GET',
+            url: '/api/curriculumVitaes'
+        }).then(function successCallback(response) {
+            var curriculumVitaes = response.data;
+            var curriculumVitaesSorted = [];
+            $.each(curriculumVitaes, function (key, value) {
+                setEmployeeName(value);
+                if (curriculumVitaesSorted.length > 0) {
+                    if (new Date(curriculumVitaesSorted[0].lastModifiedDate) < new Date(value.lastModifiedDate)) {
+                        curriculumVitaesSorted.unshift(value);
+                    } else {
+                        curriculumVitaesSorted.push(value);
+                    }
+                } else {
+                    curriculumVitaesSorted.push(value);
+                }
+            });
+            $scope.curriculumVitaesSorted = curriculumVitaesSorted;
+        }, function errorCallback(response) {
+        });
+
+        function setEmployeeName(value) {
+            $.ajax({
+                type: 'GET',
+                url: '/api/employees/' + value.employeeId,
+                success: function(data){
+                    console.log(data);
+                    value.employeeName = data.firstName + " " + data.lastName;
+                }
+            });
+        }
     });
